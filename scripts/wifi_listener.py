f4fb41c528b2d7b94a2455870d8fb697e8b82b91
John Fodero <jtfodero@gmail.com>
2017-06-20 14:43:15 -0400

    new library test script

--- scripts/wifi_listener.py
+++ scripts/wifi_listener.py
@@ -1,12 +1,24 @@
 # wifi control library
+
 from wifi import Cell
+from decimal import Decimal
+
 
 class wifi_listener():
     def __init__(self, device):
         self.device = device
 
-    def get_info(self):
+    def get_all(self):
         raw = []
-        cells = Cell.all(adapter)
-        return [cell for cell in cells].sort(key=lambda x: int(x.signal), reverse=True)
-
+        cells = Cell.all(self.device)
+        # returns a sorted list of cells sorted by RSSI
+        return sorted([cell for cell in cells], key=lambda x: int(x.signal), reverse=True)  
+    
+    def get_data(self):
+        cell_list = []
+        items = self.get_all()
+        for item in items:
+            qual = item.quality.split('/')
+            # returns a list of (MAC address, RSSI (dB), quality (%)) tuples
+            cell_list.append((item.address, int(item.signal), int('{:3.0f}'.format(100*float(qual[0])/float(qual[1]))) ))
+        return cell_list
