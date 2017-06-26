f2fafd6f86ec83bf4c7cffd68177eb7502b1cda3
John Fodero <jtfodero@gmail.com>
2017-06-26 10:17:58 -0400

    re-added capture.py

--- scripts/capture.py
+++ scripts/capture.py
@@ -1,4 +1,4 @@
-from wifi_listener import wifi_listener
+from scanner import wifi_scanner
 from time import sleep
 import datetime
 import csv
@@ -8,12 +8,14 @@ cells = 5
 sample_delay_sec = 1
 
 # to test localization
-# location 0) John's desk
-# location 1) Shiv's desk
-location = 0
-x_loc = 0
-y_loc = 0
+# location (1, 0) - John's desk
+# location (0, 1) - Shiv's desk
 
+x_loc = 1.
+y_loc = 0.
+mag_x = 0.
+mag_y = 0.
+mag_z = 0.
 file_path = '../datasets/'
 file_name = datetime.datetime.now().strftime('%Y-%m-%d_%H:%M') + 'loc' + str(location)
 img_path = 'N/A'
@@ -23,6 +25,6 @@ wifi = wifi_listener('wlp3s0')
 with open(file_path + file_name, 'wt') as csv_file:
     wr = csv.writer(csv_file, delimiter=',')
     for i in range(num_samples):
-        wr.writerow([x_loc, y_loc, img_path, *wifi.get_cells()])
+        wr.writerow([x_loc, y_loc, mag_x, mag_y, mag_z, img_path, *wifi.get_cells()])
         print('sample ', i, end='\r')
         sleep(sample_delay_sec)
