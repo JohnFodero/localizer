e942b41c191a6dc00c25717cefa4c96bdda0b928
shivrajora <rajora.shiv@gmail.com>
2017-07-24 14:40:21 -0400

    added comments

--- scripts/mapping.py
+++ scripts/mapping.py
@@ -44,12 +44,13 @@ class mapping():
         self.X_global = self.X * self.x_scale
         self.Y_global = self.Y * self.y_scale
 
-        # define colorspace
+        # define BGR colorspace dictionary
         self.color_space = {"RED" : (0,0,255), "LIME" : (0,255,0), "BLUE" : (255,0,0), "YELLOW" : (0,255,255), "CYAN" : (255,255,0), "MAGENTA" : (255,0,255), "MAROON" : (0,0,128), "OLIVE" : (0,128,128), "GREEN" : (0,128,0), "PURPLE" : (128,0,128), "TEAL" : (128,128,0), "NAVY" : (128,0,0)}
 
+        # only BGR colors
         self.colors = ((0,0,255), (0,255,0), (255,0,0), (0,255,255), (255,255,0), (255,0,255), (0,0,128), (0,128,128), (0,128,0), (128,0,128), (128,128,0), (128,0,0))
 
-    # Update the robot's location
+    # Update the robot's location in pixels
     def put_location(self, x, y):
         if(x>=self.map_width_pixel):
             print("X coordinate exceeds max dimension")
@@ -71,6 +72,7 @@ class mapping():
         self.X_global = self.X * self.x_scale
         self.Y_global = self.Y * self.y_scale
 
+    # Update the robot's location in meters
     def put_location_global(self, x_global, y_global):
         if(x_global>MAP_WIDTH_GLOBAL):
             print("X_global coordinate exceeds max dimension")
@@ -92,26 +94,29 @@ class mapping():
         self.X = floor(self.X_global / self.x_scale)
         self.Y = floor(self.Y_global / self.y_scale)
 
-    # return the robots current location
+    # return the robot's current location in pixels
     def get_location(self):
         print('Press <s> to confirm location')
         while cv2.waitKey(1) != ord('s'):
             pass
         return self.X, self.Y
 
+    # return the robot's current location in meters
     def get_location_global(self):
         print('Press <s> to confirm location')
         while cv2.waitKey(1) != ord('s'):
             pass
         return self.X_global, self.Y_global
 
+    # initialize the display
     def initialize_display(self):
         cv2.namedWindow('map', cv2.WINDOW_NORMAL)
         cv2.resizeWindow('map', self.map_width_display, self.map_height_display)
         cv2.setMouseCallback("map", self.click_and_update)
-        self.update_display(0)
+        self.__update_display(0)
 
-    def update_display(self, mode):
+    # internal function to update display
+    def __update_display(self, mode):
         # mode 0 = only show map
         if(mode == 0):
             cv2.imshow('map', self.map_img)
@@ -124,19 +129,24 @@ class mapping():
 
         cv2.waitKey(50)
     
-    def draw_kobuki(self, x, y, color=(255,0,0)):
+    # internal function to draw the kobuki's circle
+    def __draw_kobuki(self, x, y, color):
         self.map_img_kobuki = self.map_img.copy()
         cv2.circle(self.map_img_kobuki, (x, y), self.kobuki_radius_local, color, -1)
 
-    def draw_kobuki_history(self, x, y, color=(255,0,0)):
+    # internal function to draw multiple kobukis
+    def __draw_kobuki_history(self, x, y, color=(0,0,255)):
         cv2.circle(self.map_img_kobuki_history, (x, y), self.kobuki_radius_local, color, -1)
 
+    # get the obstacle bitmap
     def get_obstacle_bitmap(self):
         return self.bitmap
 
+    # change the obstacle bitmap
     def put_obstacle_bitmap(self, bitmap):
         self.bitmap = bitmap
 
+    # change the kobuki's display scale, default is 2
     def put_kobuki_scale(self, scale):
         # kobuki scale for drawing
         self.kobuki_scale = scale
@@ -144,25 +154,29 @@ class mapping():
         # Kobuki diameter in local scale
         self.kobuki_radius_local = floor(((KOBUKI_DIAMETER_GLOBAL/2)*self.kobuki_scale) / self.x_scale)
 
-    def update_kobuki(self, x, y):
+    # update the display with kobuki's new location and color (look at self.color_space dicitonary for color options)
+    def update_kobuki(self, x, y, color = (0,0,255)):
         self.put_location(x,y)
-        self.draw_kobuki(x,y)
-        self.update_display(1)
+        self.__draw_kobuki(x,y,color)
+        self.__update_display(1)
 
+    # plot the history of kobuki's oath using a list of lists. Colors will be cycled through self.colors
     def plot_path(self, coords):
         c = 0
         self.map_img_kobuki_history = self.map_img.copy()
         for i in range(len(coords)):
             for j in coords[i]:
-                self.draw_kobuki_history(j[0], j[1] , self.colors[c])
+                self.__draw_kobuki_history(j[0], j[1] , self.colors[c])
             c = c+1
             if(c==len(self.colors)):
                 c = 0
-        self.update_display(2)
+        self.__update_display(2)
 
+    # clear display
     def remove_kobuki(self):
-        self.update_display(0)
+        self.__update_display(0)
 
+    # internal funtion for mouse click updates
     def click_and_update(self, event, x, y, flags, param):
         if(event==cv2.EVENT_LBUTTONDBLCLK):
             self.update_kobuki(x,y)
@@ -174,6 +188,7 @@ class mapping():
             print("x: ", x, " y: ", y)
             print("x(m): ", x*self.x_scale, "y(m): ", y*self.y_scale)
 
+    # close display
     def close_display(self):
         cv2.waitKey(0)
         cv2.destroyAllWindows()
@@ -182,7 +197,7 @@ def main():
     M = mapping('maps/NEBfourthfloor.png', 'maps/obstacle_bitmap.bmp')
     M.initialize_display()
     M.update_kobuki(200,200)
-    coords = [[(10,10), (20, 20), (30, 30)], [(40, 40), (50, 50)], [(60,60)], [(70,70)], [(80,80)], [(90,90)], [(100,100)], [(110,110)], [(120,120), (190, 190), (200, 200), (210, 210), (220, 220)], [(130,130)], [(140,140)], [(150,150)], [(160,160)], [(170,170)], [(180,180)]]
+    coords = [[(10,10), (20, 20), (30, 30)], [(40, 40), (50, 50)], [(60,60)], [(70,70)], [(80,80)], [(90,90)], [(100,100)], [(110,110)], [(120,120), (190, 190), (200, 200), (210, 210), (220, 220)], [(130,130)], [(140,140)], [(150,150)], [(160,160)], [(170,170)], [(180,10)]]
     M.plot_path(coords)
     M.close_display()
 
